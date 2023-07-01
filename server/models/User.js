33e7fee3303ad30b184840f0e204ebe87d31bcea
Dinesh Rout <rddinesh779@gmail.com>
2023-07-02 02:31:31 +0530

    server: added user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,142 @@
 const mongoose = require('mongoose');
 
+const resumeSchema = new mongoose.Schema({
+  template_id: {
+    type: String,
+    required: true,
+  },
+  personalInfo: {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+  },
+  experience: [
+    {
+      company: {
+        name: {
+          type: String,
+          default: '',
+          required: [true, 'Company name is required'],
+        },
+        website: {
+          type: String,
+          match: /^http(s|):\/\/(www\.|)mysite\.co\?search=pattern&co=43/,
+        },
+        startDate: {
+          type: Date,
+          default: '',
+          required: [true, 'Starting Date is required'],
+        },
+        endDate: {
+          type: Date,
+          default: '',
+          required: [true, 'Ending Date is required'],
+        },
+        isCurrentJob: {
+          type: Boolean,
+          default: false,
+        },
+        jobType: {
+          type: String,
+          enum: ['Full Time', 'Part time', 'Internship'],
+          required: [true, 'Job type is required'],
+        },
+        position: {
+          type: String,
+          default: '',
+          required: [true, 'Position is required'],
+        },
+        location: {
+          type: String,
+          default: '',
+        },
+        description: {
+          type: String,
+          default: '',
+        },
+        skillsUsed: [
+          {
+            type: String,
+          },
+        ],
+        certificate: {
+          type: String,
+          match: /^http(s|):\/\/(www\.|)mysite\.co\?search=pattern&co=43/,
+        },
+      },
+    },
+  ],
+  education: [
+    {
+      degree: {
+        type: String,
+        required: true,
+      },
+      university: {
+        type: String,
+        required: true,
+      },
+      timeline: {
+        startDate: {
+          type: Date,
+        },
+        endDate: {
+          type: Date,
+        },
+      },
+      currentStudent: {
+        type: Boolean,
+      },
+      cgpa: String,
+    },
+  ],
+  skills: {
+    languages: [String],
+    tools: [String],
+    libraries: [String],
+    databses: [String],
+  },
+  projects: [
+    {
+      title: {
+        type: String,
+        required: [true, 'Project title is required'],
+      },
+      github_url: {
+        type: String,
+        match: /^http(s|):\/\/(www\.|)mysite\.co\?search=pattern&co=43/,
+      },
+      live_demo: {
+        type: String,
+        match: /^http(s|):\/\/(www\.|)mysite\.co\?search=pattern&co=43/,
+      },
+      description: [String],
+      skillsUsed: [String],
+    },
+  ],
+  achievements: [String],
+  certifications: [
+    {
+      title: String,
+      link: String,
+    },
+  ],
+});
+
+resumeSchema.pre('save', function (next) {
+  if (this.experience && this.experience.company.skillsUsed.length < 3) {
+    const error = new Error('Minimum 3 skills are required');
+    return next(error);
+  }
+  next();
+});
+
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -13,6 +150,9 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+    },
+    resume: {
+      type: resumeSchema,
     },
   },
   { timestamps: true }
