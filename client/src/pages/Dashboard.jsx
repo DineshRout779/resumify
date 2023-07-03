c88d8d4f06152690553bd866a93e2101000a6c2c
Dinesh Rout <rddinesh779@gmail.com>
2023-07-04 03:34:09 +0530

    style: minor UI changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
-import Navbar from '../components/Navbar';
+import Header from '../components/Header';
 
 const Dashboard = () => {
   return (
-    <div>
-      <Navbar />
-      This is the dashboard page.
-    </div>
+    <>
+      <Header />
+      <div className='container w-[95%] max-w-[1200px] mx-auto p-4'>
+        <h1>Choose a template</h1>
+      </div>
+    </>
   );
 };
 export default Dashboard;
