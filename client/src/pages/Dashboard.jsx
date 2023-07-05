4114e8a954bddb445fb79c3c2f65623424bc4e04
Dinesh Rout <rddinesh779@gmail.com>
2023-07-05 11:41:57 +0530

    refactor: fetch user 🎨

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,41 @@
+import { useEffect } from 'react';
 import Header from '../components/Header';
+import { useAuth } from '../hooks/useAuth';
+import { setUser, startLoading, stopLoading } from '../reducers/authReducers';
+import { isAuthenticated } from '../services/lib/auth';
 
 const Dashboard = () => {
+  const {
+    state: { loading, token, user },
+    dispatch,
+  } = useAuth();
+
+  useEffect(() => {
+    const getUser = async () => {
+      dispatch(startLoading());
+      console.log('this ran too');
+
+      try {
+        const res = await isAuthenticated();
+
+        // console.log(res);
+        localStorage.setItem('user', JSON.stringify(res.data.user));
+        dispatch(setUser(res.data.user));
+        dispatch(stopLoading());
+      } catch (error) {
+        console.log(error.response);
+        dispatch(stopLoading());
+      }
+    };
+    if (token && user === null) {
+      getUser();
+    }
+  }, [dispatch, token, user]);
+
+  if (loading) {
+    <p>Loading...please wait</p>;
+  }
+
   return (
     <>
       <Header />
