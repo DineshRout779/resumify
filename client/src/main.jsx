a49f4ae52943da23af641a2607e1504ae45bbc12
Dinesh Rout <rddinesh779@gmail.com>
2023-07-05 11:39:55 +0530

    refctor: code structure improvement 🎨

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,52 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
-import Home from './pages/Home.jsx';
-import Login from './pages/Login.jsx';
-import Signup from './pages/Signup.jsx';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Flowbite } from 'flowbite-react';
 import { customTheme } from './theme.js';
 import { Toaster } from 'react-hot-toast';
-import Dashboard from './pages/Dashboard.jsx';
-import Success from './pages/Success.jsx';
-import Error from './pages/Error.jsx';
 import AuthContextProvider from './context/AuthContext.jsx';
+import AppRoutes from './routes/AppRoutes.jsx';
+import './index.css';
 
 // Create a client
 const queryClient = new QueryClient();
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-    ],
-    errorElement: <Error />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/signup',
-    element: <Signup />,
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard />,
-  },
-  {
-    path: '/success',
-    element: <Success />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -54,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <QueryClientProvider client={queryClient}>
         <Flowbite theme={{ theme: customTheme }}>
           <Toaster position='bottom-center' reverseOrder={false} />
-          <RouterProvider router={router} />
+          <AppRoutes />
         </Flowbite>
       </QueryClientProvider>
     </AuthContextProvider>
