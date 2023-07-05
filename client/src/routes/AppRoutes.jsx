99f931fce8a0238ecea042fd4e57ea5d3b9208b9
Dinesh Rout <rddinesh779@gmail.com>
2023-07-05 11:42:12 +0530

    refactor: code structure 🎨

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from '../App';
+import Dashboard from '../pages/Dashboard';
+import Error from '../pages/Error';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+import Success from '../pages/Success';
+import PrivateRoute from './PrivateRoute';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+
+  {
+    path: '/success',
+    element: <Success />,
+  },
+  {
+    path: '/',
+    element: <PrivateRoute />,
+    children: [
+      {
+        path: 'dashboard',
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
+
+const AppRoutes = () => <RouterProvider router={router} />;
+
+export default AppRoutes;
